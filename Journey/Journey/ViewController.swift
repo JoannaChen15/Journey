e00bbe77ff7990740a39d3fda5a7b38fa0809c9b
Joanna <joufengchen15.gmail.com>
2023-07-10 15:08:57 +0800

    [chore] init commit

---
+++ Journey/Journey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journey
+//
+//  Created by 譚培成 on 2023/7/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
